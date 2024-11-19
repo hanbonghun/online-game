d9f76871801a713294e44313d00032bff746d34e
hanbonghun <bng4535@gmail.com>
2024-11-19 12:37:33 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/example/onlinegame/OnlineGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onlinegame/OnlineGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
